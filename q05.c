bb84f0fee0a6a4176b7c930fb33a664aebec2d36
guillp <guiluispaixao@gmail.com>
2026-04-21 17:52:26 -0300

    Create q05.c

---
+++ q05.c
@@ -0,0 +1,15 @@
+#include  <stdio.h>
+
+int main() {
+    int valor;
+
+
+    printf("Escreva o seu numero: \n",valor);
+    scanf("%d", &valor);
+
+    int valor_total = valor + 1;
+
+    printf("O seu sucessor vai ser: %d\n",valor_total);
+
+    return 0;
+}
