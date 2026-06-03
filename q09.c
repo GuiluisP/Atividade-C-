ad405636435df66de448fc8bccd6d787d78f3fbf
guillp <guiluispaixao@gmail.com>
2026-06-02 21:27:29 -0300

    Create q09.c

---
+++ q09.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int valor1 , valor2;
+
+    printf("Escreva o seu primeiro valor: \n");
+    scanf("%d", &valor1);
+
+    printf("Escreva o seu segundo valor: \n");
+    scanf("%d", &valor2);
+
+    int resto = valor1 / valor2;
+
+    printf("O seu resultado da divisao e: %d\n", resto);
+
+    return 0;
+}
